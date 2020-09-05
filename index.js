af7ddd12ea418514ac99f9607bcfad9254d1c62d
Collier <collier.bell20@gmail.com>
2020-09-05 17:28:27 -0500

    Our first command, plus some fixes .travis.yml

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-require('dotenv').config();;
-const Discord = require('discord.js');
-const client = new Discord.Client();
+require('dotenv').config();
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
+
+const client = new CommandoClient({
+    commandPrefix: 's?',
+    owner: '573909482619273255',
+    invite: 'https://discord.gg/bRCvFy9',
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['user', 'User commands.'],
+        ['admin', 'Admin commands.'],
+        ['developer', 'Developer commands.']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.once("ready", () => {
     console.log(`${client.user.username} logged in!`)
+    client.user.setActivity(`with ${client.users.size} other users!`);
 });
 
+client.on('error', console.error);
 client.login();
